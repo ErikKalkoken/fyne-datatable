291de28e6fe9b0f707f7a82b101c6f589a398c58
ErikKalkoken <erik.kalkoken@gmail.com>
2024-09-30 14:57:05 +0200

    Add mutex to make it thread safe

--- datatable.go
+++ datatable.go
@@ -1,8 +1,10 @@
 package datatable
 
 import (
+	"fmt"
 	"slices"
 	"strings"
+	"sync"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
@@ -12,42 +14,55 @@ import (
 
 type DataTable struct {
 	widget.BaseWidget
+	bottomLabel *widget.Label
+	header      []fyne.CanvasObject
+	headerCells []string
+	list        *widget.List
+	numCols     int
+
+	mu            sync.RWMutex
+	layout        columnsLayout
 	cells         [][]string
 	cellsFiltered [][]string
-	header        []string
-	headerRow     []fyne.CanvasObject
-	layout        columnsLayout
-	list          *widget.List
 }
 
-func NewDataTable(header []string) *DataTable {
-	w := &DataTable{header: header}
+func NewDataTable(headerCells []string) *DataTable {
+	w := &DataTable{
+		bottomLabel: widget.NewLabel(""),
+		headerCells: headerCells,
+		numCols:     len(headerCells),
+	}
 	w.ExtendBaseWidget(w)
 	w.list = w.makeList()
-	w.headerRow = w.makeHeaderRow()
+	w.header = w.makeHeader()
 	return w
 }
 
 func (w *DataTable) makeList() *widget.List {
 	list := widget.NewList(
 		func() int {
+			w.mu.RLock()
+			defer w.mu.RUnlock()
 			return len(w.cellsFiltered)
 		},
 		func() fyne.CanvasObject {
-			c := len(w.header)
-			objects := make([]fyne.CanvasObject, c)
-			for i := range c {
+			w.mu.RLock()
+			defer w.mu.RUnlock()
+			objects := make([]fyne.CanvasObject, w.numCols)
+			for i := range w.numCols {
 				objects[i] = widget.NewLabel("")
 			}
 			return container.New(w.layout, objects...)
 		},
 		func(id widget.ListItemID, co fyne.CanvasObject) {
+			w.mu.RLock()
+			defer w.mu.RUnlock()
 			if id >= len(w.cellsFiltered) {
 				return // safeguard
 			}
 			r := w.cellsFiltered[id]
 			c := co.(*fyne.Container)
-			for i := range len(w.header) {
+			for i := range w.numCols {
 				o := c.Objects[i].(*widget.Label)
 				o.SetText(r[i])
 			}
@@ -56,53 +71,54 @@ func (w *DataTable) makeList() *widget.List {
 	return list
 }
 
-func (w *DataTable) makeHeaderRow() []fyne.CanvasObject {
-	filterRows := func(filter []string) {
-		var selection [][]string
-		for _, row := range w.cells {
-			match := true
-			for i, c := range row {
-				c2 := strings.ToLower(c)
-				if filter[i] != "" && !strings.Contains(c2, strings.ToLower(filter[i])) {
-					match = false
-					break
-				}
-			}
-			if match {
-				selection = append(selection, row)
-			}
-		}
-		w.cellsFiltered = selection
-		w.list.Refresh()
-	}
-	objects := make([]fyne.CanvasObject, len(w.header))
-	for i, s := range w.header {
+func (w *DataTable) makeHeader() []fyne.CanvasObject {
+	objects := make([]fyne.CanvasObject, w.numCols)
+	for i, s := range w.headerCells {
 		o := widget.NewEntry()
 		o.PlaceHolder = s
 		o.OnChanged = func(s string) {
-			filter := make([]string, len(w.header))
+			filter := make([]string, w.numCols)
 			for j, x := range objects {
 				y := x.(*widget.Entry)
 				filter[j] = y.Text
 			}
 			filter[i] = s
-			filterRows(filter)
+			w.filterRows(filter)
+			w.list.Refresh()
 		}
 		objects[i] = o
 	}
 	return objects
 }
 
-func (w *DataTable) SetCells(cells [][]string) {
-	w.cells = cells
-	w.cellsFiltered = slices.Clone(cells)
-	w.layout = columnsLayout(maxColWidths(cells))
+func (w *DataTable) filterRows(filter []string) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	var selection [][]string
+	for _, row := range w.cells {
+		match := true
+		for i, c := range row {
+			c2 := strings.ToLower(c)
+			if filter[i] != "" && !strings.Contains(c2, strings.ToLower(filter[i])) {
+				match = false
+				break
+			}
+		}
+		if match {
+			selection = append(selection, row)
+		}
+	}
+	w.cellsFiltered = selection
 }
 
-func (w *DataTable) CreateRenderer() fyne.WidgetRenderer {
-	head := container.NewVBox(container.New(w.layout, w.headerRow...), widget.NewSeparator())
-	c := container.NewBorder(head, nil, nil, nil, w.list)
-	return widget.NewSimpleRenderer(c)
+func (w *DataTable) SetCells(cells [][]string) {
+	defer w.list.Refresh()
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	w.cells = slices.Clone(cells)
+	w.cellsFiltered = slices.Clone(cells)
+	w.layout = columnsLayout(maxColWidths(slices.Concat([][]string{w.headerCells}, cells)))
+	w.bottomLabel.SetText(fmt.Sprintf("%d rows", len(w.cells)))
 }
 
 func maxColWidths(cells [][]string) []float32 {
@@ -118,6 +134,15 @@ func maxColWidths(cells [][]string) []float32 {
 		}
 	}
 	return colWidths
+}
+
+func (w *DataTable) CreateRenderer() fyne.WidgetRenderer {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	header := container.NewVBox(container.New(w.layout, w.header...), widget.NewSeparator())
+	footer := container.NewVBox(widget.NewSeparator(), w.bottomLabel)
+	c := container.NewBorder(header, footer, nil, nil, w.list)
+	return widget.NewSimpleRenderer(c)
 }
 
 type columnsLayout []float32
